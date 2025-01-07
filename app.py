3d6b61b4ac5538efb56480c8800ef83512e1c1dd
Aurora Jitrskul <aurawra@outlook.com>
2025-01-07 16:03:40 -0600

    Preparing server for deployment

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO
-import json
 
 # Initialize the Flask app
 app = Flask(__name__)
@@ -51,15 +50,4 @@ def end_game():
     socketio.emit("end_game")
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
-    #socketio.run(app, host='0.0.0.0', port=80)
-
-
-
-# @app.route('/vote')
-# def receiveVote():
-#   return True
-
-# #Run Flask server
-# if __name__ == "__main__":
-#   app.run(host="127.0.0.1", port=5001, debug=True)
+    socketio.run(app, host='0.0.0.0', port=80)
