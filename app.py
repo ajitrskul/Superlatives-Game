f9741a0988c24f42b896a3271226fe7b9844ebfe
Aurora Jitrskul <aurawra@outlook.com>
2025-01-07 18:21:02 -0600

    Added eventlet and changed to utilizing port env var

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO
+import eventlet
+import os
 
 # Initialize the Flask app
 app = Flask(__name__)
-socketio = SocketIO(app)
+socketio = SocketIO(app, async_mode='eventlet')
 
 @app.route('/')
 def client():
@@ -50,4 +52,5 @@ def end_game():
     socketio.emit("end_game")
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=80)
+    port = int(os.environ.get("PORT", 5000))
+    socketio.run(app, host="0.0.0.0", port=port)
